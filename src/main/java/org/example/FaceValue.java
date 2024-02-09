471be9d5272ec789231338ca2cedec593cc84aeb
UsernameForAClass <bra2306725@maricopa.edu>
2024-02-08 19:32:14 -0700

    Adding the activity cards

---
+++ src/main/java/org/example/FaceValue.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public enum FaceValue {
+
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+
+}
---
+++ src/main/java/org/example/Suit.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public enum Suit {
+
+    HEARTS, CLUBS , DIAMONDS, SPADES
+
+}
